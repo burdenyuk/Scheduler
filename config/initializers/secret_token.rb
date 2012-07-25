3d1766b66f5f7e7635ff8f300bf26244ac5854aa
Denis Burdenyuk <burdenyukdenis@gmail.com>
2012-07-25 23:30:11 +0300

    Skeleton (mysql, slim, twitter bootstrap, rspec, annotate  gems included)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scheduler::Application.config.secret_token = '252ca146a47b521cf3ad60d1797606328091fa912b900ac530e0c9ae5adda39d8a9b17e0839e82df8ed75dbc5f11cc8bac538054e2e9182f26ebeded132b2b08'
